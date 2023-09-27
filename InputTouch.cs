8bd45cf0f7ede8f0b43cd1f9224e53140b853c97
RileyA <scout1553@gmail.com>
2023-09-26 18:03:48 -0600

    more touch

--- InputTouch.cs
+++ InputTouch.cs
@@ -1,22 +1,21 @@
 using UnityEngine;
-using UnityEngine.Events;
 using UnityEngine.InputSystem;
 
 
 public class InputTouch : MonoBehaviour
 {
-   [SerializeField] 
+   [SerializeField]
    private GameObject player;
-   
    private PlayerInput playerInput;
-   private InputAction touchPositionAction;
+   
    private InputAction touchPressAction;
-
+   private InputAction touchPositionAction;
+   
    private void Awake()
    {
       playerInput = GetComponent<PlayerInput>();
-      touchPressAction = playerInput.actions("TouchPress");
-      touchPositionAction = playerInput.actions("TouchPosition");
+      touchPressAction = playerInput.actions["TouchPress"];
+      touchPositionAction = playerInput.actions["TouchPosition"];
    }
    
    private void OnEnable()
@@ -24,16 +23,16 @@ public class InputTouch : MonoBehaviour
       touchPressAction.performed += TouchPress;
       //touchPositionAction.performed += OnTouchPosition;
    }
-   
    private void OnDisable()
    {
       touchPressAction.performed -= TouchPress;
       //touchPositionAction.performed -= OnTouchPosition;
    }
+   
    private void TouchPress(InputAction.CallbackContext context)
    {
-    Vector3 = Camera.main.ScreenToWorldPoint(touchPositionAction.ReadValue<Vector2>());
-    position.z = player.transform.position.z;
-    player.transform.position = posision;
+     Vector3 position = Camera.main.ScreenToWorldPoint(touchPositionAction.ReadValue<Vector2>());
+     position.z = player.transform.position.z;
+     player.transform.position = position;
    }
 }
