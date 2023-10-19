9347686ebf250efa24577600970d1f3c965915f4
Riley Anderson <10803452@uvu.edu>
2023-10-18 22:13:10 -0600

    Still figuring out spawning

--- BrickSpawn.cs
+++ BrickSpawn.cs
@@ -8,35 +8,43 @@ public class BrickSpawn : MonoBehaviour
     public MaterialList materialsList;
     public Dictionary<Material, int> materialHealthMap;
 
-    //public void BrickSpawner()
-    void Start()
+    
+    public void SpawnBricks()
     {
         // Spawn an object at each position in the list
         foreach (Vector3 spawnPosition in spawnPositionsList.positions)
         {
             // Instantiate the object at the position
             GameObject spawnedObject = Instantiate(objectPrefab, spawnPosition, Quaternion.identity);
-            //Instantiate(objectPrefab[num], spawnPosition, Quaternion.identity);
 
             // Get a random material from the list
             Material material = materialsList.GetRandomMaterial();
 
             // Set the object's material
-            Renderer rend = objectPrefab.GetComponent<Renderer>();
+            Renderer rend = spawnedObject.GetComponent<Renderer>();
             if (rend != null)
             {
                 rend.material = material;
             }
+            
 
             // Set the object's health based on the material
-            int health;
-            if (materialHealthMap.TryGetValue(material, out health))
+            if (materialHealthMap.TryGetValue(material, out int health))
             {
                 Brick healthComponent = spawnedObject.GetComponent<Brick>();
-                if (healthComponent != null)
+                if (healthComponent == null)
+                {
+                    Debug.LogError("The spawned object does not have a Brick component for health.");
+                }
+                else
                 {
                     healthComponent.SetHealth(health);
+                    Debug.LogError("Health set to: " + health);
                 }
+            }
+            else
+            {
+                Debug.LogError("No health value found for the material: " + material.name);
             }
         }
     }
