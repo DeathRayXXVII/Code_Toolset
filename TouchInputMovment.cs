fae84d0965bde1daee73f358aca6f92767d06fc4
Riley Anderson <10803452@uvu.edu>
2023-10-20 19:21:51 -0600

    trying to figure out touch

---
+++ TouchInputMovment.cs
@@ -0,0 +1,37 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class TouchInputMovement : MonoBehaviour
+{
+    public floatData speed;
+    public GameInputsSO controls;
+    private Vector2 touchStartPosition;
+    private bool isTouching;
+
+    void Update()
+    {
+        if (isTouching)
+        {
+            Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+            float touchDelta = touchPosition.x - touchStartPosition.x;
+            transform.position += new Vector3(touchDelta, 0f, 0f) * (speed.value * Time.deltaTime);
+        }
+    }
+
+    public void OnTouchStart(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            touchStartPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+            isTouching = true;
+        }
+    }
+
+    public void OnTouchEnd(InputAction.CallbackContext context)
+    {
+        if (context.phase == InputActionPhase.Started)
+        {
+            isTouching = false;
+        }
+    }
+}
