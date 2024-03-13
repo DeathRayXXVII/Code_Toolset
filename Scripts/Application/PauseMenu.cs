24b4f1051dafdb6ebdfb96c7b7b853d17b8cf481
Riley Anderson <10803452@uvu.edu>
2024-03-12 21:05:05 -0600

    Testing out ways to more access the Pause and a have a delay action

--- Scripts/Application/PauseMenu.cs
+++ Scripts/Application/PauseMenu.cs
@@ -1,3 +1,5 @@
+using System;
+using System.Collections;
 using UnityEngine;
 using UnityEngine.Events;
 
@@ -6,7 +8,9 @@ namespace Scripts
     public class PauseMenu : MonoBehaviour
     {
         public bool gameIsPaused;
-        public float seconds;
+        public bool delayPause;
+        public bool bypassDelay;
+        public float delay = 1f;
         public UnityEvent startEvent, resumeEvent;
 
         public bool GameIsPaused
@@ -17,6 +21,18 @@ namespace Scripts
         private void Start()
         {
             startEvent.Invoke();
+            DelayPause();
+        }
+
+        private void Update()
+        {
+            if (bypassDelay)
+            {
+                //yield return new WaitForSeconds(delay);
+                delayPause = false;
+                GameIsPaused = false;
+                Time.timeScale = 1f;
+            }
         }
 
         public void StartResume()
@@ -27,6 +43,31 @@ namespace Scripts
 
         public void StartPause()
         {
+            GameIsPaused = true;
+            Time.timeScale = 0f;
+        }
+        public void BypassDelay()
+        {
+            bypassDelay = true;
+        }
+        
+        public void BypassDelayOff()
+        {
+            bypassDelay = false;
+        }
+        
+        public void DelayPause()
+        {
+            if (delayPause)
+            {
+                StartCoroutine(StartDelay());
+            }
+        }
+        
+        IEnumerator StartDelay()
+        {
+            yield return new WaitForSeconds(delay);
+            delayPause = false;
             GameIsPaused = true;
             Time.timeScale = 0f;
         }
