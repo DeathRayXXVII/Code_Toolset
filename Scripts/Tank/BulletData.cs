8be2579b23cc6e736a52a14f85e654ccc52a83fb
Riley Anderson <10803452@uvu.edu>
2024-07-17 17:26:17 -0600

    Redoing Enemytank

--- Scripts/Tank/BulletData.cs
+++ Scripts/Tank/BulletData.cs
@@ -5,7 +5,6 @@ public class BulletData : ScriptableObject
 {
     public GameObject shellPrefab;
     public float speed = 10.0f;
-    public float rotationSpeed = 2.0f;
     public float maxBounces = 2.0f;
     public float damage = 10.0f;
     public float timeBetweenShots = 0.5f;
--- Scripts/Tank/Enemytank.cs
+++ Scripts/Tank/Enemytank.cs
@@ -3,160 +3,68 @@ using UnityEngine.AI;
 
 public class EnemyTank : MonoBehaviour
 {
-    [SerializeField] private NavMeshAgent navAgent;
+    [SerializeField] private NavMeshAgent agent;
     [SerializeField] private Transform player;
-    [SerializeField] private LayerMask groundLayer, playerLayer;
+    [SerializeField] private BulletData bulletData;
+    [SerializeField] private BulletBehavior bB;
+    [SerializeField] private float speed;
     [SerializeField] private float walkPointRange;
-    [SerializeField] private float timeBetweenAttacks;
+    [SerializeField] private float attackSpeed;
+    [SerializeField] private float roationSpeed;
+    [SerializeField] private Transform fireTransform;
+    [SerializeField] private GameObject barrelPrefab;
+    [SerializeField] private bool stationary;
+    [SerializeField] private LayerMask groundLayer, playerLayer;
     [SerializeField] private float sightRange;
     [SerializeField] private float attackRange;
 
     private Vector3 walkPoint;
     private bool walkPointSet;
-    private bool alreadyAttacked;
-    private bool takeDamage;
-    
-    [SerializeField] private GameObject barrelPrefab;
-    [SerializeField] private Transform fireTransform;
-    [SerializeField] private BulletBehavior bB;
-    [SerializeField] private BulletData bulletData;
-    [SerializeField] private float attackSpeed = 1f;
     private float timer;
-    [SerializeField] private bool stationary = false;
-    private bool isPatrolling = false;
-    private bool isStationary = false;
-
-    void Awake()
+    
+    private void Awake()
     {
         player = GameObject.Find("Player").transform;
-        navAgent = GetComponent<NavMeshAgent>();
+        agent = GetComponent<NavMeshAgent>();
+        
         if (stationary)
         {
-            isStationary = true;
-            isPatrolling = false;
-        }
-        else
-        {
-            isStationary = false;
-            isPatrolling = true;
+            agent.updatePosition = false;
+            //agent.updateRotation = false;
         }
     }
-
-    void Update()
+    
+    private void Update()
     {
         var position = transform.position;
         float distanceToTarget = Vector3.Distance(player.position, position);
-        timer += Time.deltaTime;
-
         bool playerInSightRange = Physics.CheckSphere(position, sightRange, playerLayer);
         bool playerInAttackRange = Physics.CheckSphere(position, attackRange, playerLayer);
-
-        if (isPatrolling)
+        if (!stationary)
         {
-            if (!playerInSightRange && !playerInAttackRange)
-            {
-                Patroling();
-            }
-            else if (playerInSightRange && !playerInAttackRange)
-            {
-                ChasePlayer();
-                RotateBarrel();
-            }
-            else if(playerInSightRange)
-            {
-                //ChasePlayer();
-                if (!CanSeePlayer())
-                {
-                    //AttemptRicochetShot();
-                    AttemptRicochetDetection();
-                }
-                else
-                {
-                    RotateBarrel();
-                    if (timer >= attackSpeed)
-                    {
-                        AttackPlayer();
-                        timer = 0f;
-                    }
-                }
-            }
+           if (!playerInSightRange && !playerInAttackRange)
+           {
+               if (!walkPointSet)
+               {
+                   SearchWalkPoint();
+               }
+               else
+               {
+                   agent.SetDestination(walkPoint);
+               }
+           }
+                   
+           if (playerInSightRange && !playerInAttackRange)
+           {
+               FollowPlayer();
+               RotateBarrel();
+           }
         }
-
-        if (!isStationary) return;
-        if (playerInSightRange)
+        if (playerInSightRange && playerInAttackRange)
         {
-            AttemptRicochetDetection();
-        }
-        /*if (!playerInSightRange && playerInAttackRange && distanceToTarget <= attackRange)
-        {
-            if (!CanSeePlayer())
-            {
-                AttemptRicochetShot();
-            }
-            else
-            {
-                RotateBarrel();
-            }
-        }
-        else if (playerInSightRange && !playerInAttackRange && distanceToTarget <= attackRange)
-        {
-            if (!CanSeePlayer())
-            {
-                AttemptRicochetShot();
-            }
-        }
-        else if (playerInSightRange && distanceToTarget <= attackRange)
-        {
-            if (CanSeePlayer())
-            {
-                RotateBarrel();
-            }
-            if (timer >= attackSpeed)
-            {
-                AttackPlayer();
-                timer = 0f;
-            }
-        }*/
-
-
-
-        /*if (distanceToTarget <= attackRange)
-        {
-            if (!CanSeePlayer())
-            {
-                
-            }
-            else
-            {
-                RotateBarrel();
-            }
-        }
-        else
-        {
-            if (!isStationary)
-            {
-                navAgent.SetDestination(target.position);
-            }
-        }*/
-    }
-    
-    private void Patroling()
-    {
-        if (!walkPointSet)
-        {
-            SearchWalkPoint();
-        }
-        else
-        {
-            navAgent.SetDestination(walkPoint);
-        }
-
-        Vector3 distanceToWalkPoint = transform.position - walkPoint;
-
-        if (distanceToWalkPoint.magnitude < 1f)
-        {
-            walkPointSet = false;
-        }
+            AttackPlayer();
+            RotateBarrel();
+        } 
     }
     
     private void SearchWalkPoint()
@@ -166,158 +74,48 @@ public class EnemyTank : MonoBehaviour
 
         var position = transform.position;
         walkPoint = new Vector3(position.x + randomX, position.y, position.z + randomZ);
-
+        
         if (Physics.Raycast(walkPoint, -transform.up, 2f, groundLayer))
         {
             walkPointSet = true;
         }
     }
     
-    private void ChasePlayer()
+    private void FollowPlayer()
     {
-        navAgent.SetDestination(player.position);
+        agent.SetDestination(player.position);
+        //barrelPrefab.transform.LookAt(player);
     }
     
     private void AttackPlayer()
     {
-        navAgent.SetDestination(transform.position);
-
-        //transform.LookAt(player);
-
-        if (!alreadyAttacked)
+        agent.SetDestination(player.position);
+        //barrelPrefab.transform.LookAt(player);
+        
+        if (Vector3.Distance(transform.position, player.position) <= attackSpeed)
         {
-            // Attack code here
-            // Add shooting or other attack logic here
-            Vector3 direction = fireTransform.up;
-            BulletBehavior bullet = Instantiate(bB, fireTransform.position, fireTransform.rotation);
-            bullet.Shoot(direction.normalized);
-
-            alreadyAttacked = true;
-            Invoke(nameof(ResetAttack), timeBetweenAttacks);
+            if (timer >= attackSpeed)
+            {
+                Vector3 direction = fireTransform.up;
+                BulletBehavior bullet = Instantiate(bB, fireTransform.position, fireTransform.rotation);
+                bullet.Shoot(direction.normalized);
+                timer = 0f;
+            }
         }
     }
-    
-    private void ResetAttack()
-    {
-        alreadyAttacked = false;
-    }
-    
+
     private void RotateBarrel()
     {
         Vector3 targetPosition = player.position;
         var position = barrelPrefab.transform.position;
         targetPosition.y = position.y;
         Vector3 direction = targetPosition - position;
-        
+
         Quaternion lookRotation = Quaternion.LookRotation(direction);
         lookRotation *= Quaternion.Euler(0, 90, 0);
         barrelPrefab.transform.rotation = Quaternion.Slerp(barrelPrefab.transform.rotation, lookRotation, Time.deltaTime * 5f);
     }
-    
-    private bool CanSeePlayer()
-    {
-        RaycastHit hit;
-        Vector3 direction = player.position - transform.position;
-        if (Physics.Raycast(transform.position, direction, out hit))
-        {
-            if (hit.transform == player)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    /*
-    private void AttemptRicochetShot()
-    {
-        bool shotSuccessful = false;
-        float bestDistance = float.MaxValue;
-        Vector3 bestRicochetPoint = Vector3.zero;
 
-        // Step 1 & 2: Optimize raycast angles and improve ricochet point selection
-        for (int angle = -60; angle <= 60; angle += 5)
-        {
-            Vector3 scanDirection = Quaternion.Euler(0, angle, 0) * transform.forward;
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, scanDirection, out hit, 100f))
-            {
-                Vector3 ricochetDirection = Vector3.Reflect(scanDirection, hit.normal);
-                RaycastHit secondHit;
-                if (Physics.Raycast(hit.point, ricochetDirection, out secondHit))
-                {
-                    float distanceToTarget = Vector3.Distance(secondHit.point, player.position);
-                    if (secondHit.transform == player && distanceToTarget < bestDistance)
-                    {
-                        bestDistance = distanceToTarget;
-                        bestRicochetPoint = hit.point;
-                        shotSuccessful = true;
-                    }
-                }
-            }
-        }
-
-        // Step 3 & 4: Calculate ricochet angles more precisely and verify clear path
-        if (shotSuccessful)
-        {
-            AimAtPoint(bestRicochetPoint);
-            AttackPlayer();
-        }
-    }
-    */
-    
-    private void AttemptRicochetDetection()
-    {
-        if (!CanSeePlayer() && Physics.CheckSphere(transform.position, sightRange, playerLayer))
-        {
-            RaycastHit hit;
-            // Cast a ray around the tank to find a potential ricochet point
-            for (int angle = 0; angle < 360; angle += 10)
-            {
-                Vector3 direction = Quaternion.Euler(0, angle, 0) * transform.forward;
-                if (Physics.Raycast(transform.position, direction, out hit, sightRange))
-                {
-                    // Simulate the bullet's trajectory considering its maximum bounces
-                    if (SimulateRicochet(hit.point, direction, bulletData.maxBounces))
-                    {
-                        // If a valid path to the player is found, aim towards the first ricochet point
-                        AimAtPoint(hit.point);
-                        AttackPlayer();
-                        break;
-                    }
-                }
-            }
-        }
-    }
-
-    private bool SimulateRicochet(Vector3 position, Vector3 direction, float maxBounces)
-    {
-        if (maxBounces <= 0) return false;
-
-        RaycastHit hit;
-        if (Physics.Raycast(position, direction, out hit, sightRange))
-        {
-            // Check if the ray hit the player
-            if (hit.transform == player)
-            {
-                return true;
-            }
-
-            // Calculate the next ricochet direction
-            Vector3 nextDirection = Vector3.Reflect(direction, hit.normal);
-            // Recursively simulate the next bounce
-            return SimulateRicochet(hit.point, nextDirection, maxBounces - 1);
-        }
-        return false;
-    }
-    
-    private void AimAtPoint(Vector3 point)
-    {
-        Vector3 direction = point - barrelPrefab.transform.position;
-        Quaternion lookRotation = Quaternion.LookRotation(direction);
-        barrelPrefab.transform.rotation = Quaternion.Slerp(barrelPrefab.transform.rotation, lookRotation, Time.deltaTime * bulletData.rotationSpeed);
-    }
-    
     private void OnDrawGizmosSelected()
     {
         Gizmos.color = Color.red;
@@ -326,142 +124,4 @@ public class EnemyTank : MonoBehaviour
         Gizmos.color = Color.yellow;
         Gizmos.DrawWireSphere(position, sightRange);
     }
-    
-    
-    
-    /*private NavMeshAgent agent;
-    [SerializeField] private Transform target;
-    [SerializeField] private GameObject barrelPrefab;
-    [SerializeField] private Transform fireTransform;
-    [SerializeField] private BulletData bulletData;
-    [SerializeField] private float attackSpeed = 1f;
-    private float timer;
-    [SerializeField] private float attackRange = 10f;
-    [SerializeField] private bool stationary = false;
-    private bool isStationary = false;
-
-    void Awake()
-    {
-        if (stationary)
-        {
-            isStationary = true;
-        }
-        else
-        {
-            isStationary = false;
-        }
-        agent = GetComponent<NavMeshAgent>();
-    }
-
-    void Update()
-    {
-        float distanceToTarget = Vector3.Distance(target.position, transform.position);
-        timer += Time.deltaTime;
-        if (distanceToTarget <= attackRange)
-        {
-            if (!CanSeePlayer())
-            {
-                AttemptRicochetShot();
-            }
-            else
-            {
-                RotateBarrel();
-            }
-            if (timer >= attackSpeed)
-            {
-               Fire();
-               timer = 0f; 
-            }    
-        }
-        else
-        {
-            if (!isStationary)
-            {
-                agent.SetDestination(target.position);
-            }
-        }
-    }
-    
-    private void RotateBarrel()
-    {
-        Vector3 targetPosition = target.position;
-        var position = barrelPrefab.transform.position;
-        targetPosition.y = position.y;
-        Vector3 direction = targetPosition - position;
-        
-        Quaternion lookRotation = Quaternion.LookRotation(direction);
-        lookRotation *= Quaternion.Euler(0, 90, 0);
-        barrelPrefab.transform.rotation = Quaternion.Slerp(barrelPrefab.transform.rotation, lookRotation, Time.deltaTime * 5f);
-    }
-    
-    private bool CanSeePlayer()
-    {
-        RaycastHit hit;
-        Vector3 direction = target.position - transform.position;
-        if (Physics.Raycast(transform.position, direction, out hit))
-        {
-            if (hit.transform == target)
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    private void Fire()
-    {
-        // Add shooting or other attack logic here
-        Quaternion correctedRotation = fireTransform.rotation * Quaternion.Euler(0, -90, 0);
-        Instantiate(bulletData.shellPrefab, fireTransform.position, correctedRotation);
-        bulletData.shellPrefab.transform.rotation = correctedRotation;
-    }
-    
-        private void AttemptRicochetShot()
-    {
-        bool shotSuccessful = false;
-        float bestDistance = float.MaxValue;
-        Vector3 bestRicochetPoint = Vector3.zero;
-
-        // Step 1 & 2: Optimize raycast angles and improve ricochet point selection
-        for (int angle = -60; angle <= 60; angle += 5)
-        {
-            Vector3 scanDirection = Quaternion.Euler(0, angle, 0) * transform.forward;
-            RaycastHit hit;
-            if (Physics.Raycast(transform.position, scanDirection, out hit, 100f))
-            {
-                Vector3 ricochetDirection = Vector3.Reflect(scanDirection, hit.normal);
-                RaycastHit secondHit;
-                if (Physics.Raycast(hit.point, ricochetDirection, out secondHit))
-                {
-                    float distanceToTarget = Vector3.Distance(secondHit.point, target.position);
-                    if (secondHit.transform == target && distanceToTarget < bestDistance)
-                    {
-                        bestDistance = distanceToTarget;
-                        bestRicochetPoint = hit.point;
-                        shotSuccessful = true;
-                    }
-                }
-            }
-        }
-
-        // Step 3 & 4: Calculate ricochet angles more precisely and verify clear path
-        if (shotSuccessful)
-        {
-            AimAtPoint(bestRicochetPoint);
-            Fire();
-        }
-    }
-
-    private void AimAtPoint(Vector3 point)
-    {
-        Vector3 direction = point - barrelPrefab.transform.position;
-        Quaternion lookRotation = Quaternion.LookRotation(direction);
-        barrelPrefab.transform.rotation = Quaternion.Slerp(barrelPrefab.transform.rotation, lookRotation, Time.deltaTime * 5f);
-    }
-    
-    private void OnDrawGizmos()
-    {
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(transform.position, attackRange);
-    }*/
 }
